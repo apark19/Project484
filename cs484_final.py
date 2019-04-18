79a9f922d83dc3662f0d8dbce369f1a05d3169aa
Andrew Park <apark19@gmu.edu>
2019-04-18 12:33:47 -0400

    Add files via upload

--- cs484_final.py
+++ cs484_final.py
@@ -1,24 +1,27 @@
 import nltk  # the variable corpus is the training set
 import re
-import numpy as np
-import sklearn
-import time
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy import sparse
-from operator import itemgetter
 from multiprocessing import Process, Lock, Pool, freeze_support
 import time
-from nltk import pos_tag
 import pandas as pd
 import copy
 from nltk.corpus import stopwords
-from functools import partial
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
 
 
+# Unused imports thus far...
+#import numpy as np
+#import sklearn
+#import time
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.metrics.pairwise import cosine_similarity
+#from scipy import sparse
+#from operator import itemgetter
+#from nltk import pos_tag
+#from functools import partial
+#from nltk.tokenize import word_tokenize
+#from nltk.stem import PorterStemmer
+
+lemmatizer = nltk.stem.WordNetLemmatizer
 stpwrd = set(stopwords.words('english'))
 stpwrd_update = ["br","eof","i","im","i'm",",","it","as","the","this","but","its","it's","you" "also","for","us","was","to","on","there","of","in","his","hers","-"]
 stpwrd.update(stpwrd_update)
@@ -39,7 +42,7 @@ def Pre(par):
     paragraph = nltk.pos_tag(paragraph) # returns tuples with each word paired with parts of speech keyword https://medium.com/@gianpaul.r/tokenization-and-parts-of-speech-pos-tagging-in-pythons-nltk-library-2d30f70af13b
     new_paragraph = [word[0] for word in paragraph if((word[0] not in stpwrd) and (word[1] in pos_keep))] # the first condition is uneccesary if the second works properly
 
-    new_paragraph= " ".join(new_paragraph)
+    new_paragraph = " ".join(new_paragraph)
     tmp[2] = new_paragraph
     # once the issue is fixed, I would like to see if lemmatization could be implemented while maintainig reasonable runtime https://www.geeksforgeeks.org/python-lemmatization-with-nltk/
     return tmp
