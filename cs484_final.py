fbcb05534cd7073d527899d3ff0703a7f976872a
3A-2Z <48304266+3A-2Z@users.noreply.github.com>
2019-04-18 23:22:29 -0400

    Update cs484_final.py

--- cs484_final.py
+++ cs484_final.py
@@ -1,73 +1,89 @@
-import nltk  # the variable corpus is the training set
+import nltk#the variable corpus is the training set
 import re
-from multiprocessing import Process, Lock, Pool, freeze_support
+import numpy as np
+import sklearn
 import time
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import sparse
+from operator import itemgetter
+from multiprocessing import Process,Lock,Pool,freeze_support
+import time
+from nltk import pos_tag
 import pandas as pd
 import copy
 from nltk.corpus import stopwords
+from functools import partial
 
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 
-# Unused imports thus far...
-#import numpy as np
-#import sklearn
-#import time
-#from sklearn.feature_extraction.text import TfidfVectorizer
-#from sklearn.feature_extraction.text import CountVectorizer
-#from sklearn.metrics.pairwise import cosine_similarity
-#from scipy import sparse
-#from operator import itemgetter
-#from nltk import pos_tag
-#from functools import partial
-#from nltk.tokenize import word_tokenize
-#from nltk.stem import PorterStemmer
-
-lemmatizer = nltk.stem.WordNetLemmatizer
 stpwrd = set(stopwords.words('english'))
 stpwrd_update = ["br","eof","i","im","i'm",",","it","as","the","this","but","its","it's","you" "also","for","us","was","to","on","there","of","in","his","hers","-"]
 stpwrd.update(stpwrd_update)
-
-
+lemmatizer = nltk.stem.WordNetLemmatizer
 def Pre(par):
-    par = list(par)
-    tmp = copy.deepcopy(par)
-    paragraph = tmp[2].lower() # the plots are in the third element of the array. lower is used so that words will still be considered the same instance when vectorized
-    paragraph = re.sub('[^+A-Za-z0-9]+',' ',paragraph) # substitutes non alphabetic or numerical characters with a space
-    paragraph = re.split(' ',paragraph)
-    pos_keep = ['JJ','JJR','JJR','JJS','RB','RBR','RBS','VB','VBD','VBG','VBN','VBP','VBZ'] # all desired parts of speech (all forms of verbs, adhectives etc. no subject)
-
-    if '' in paragraph: # removes any empty character within a passed paragraph that may be left over
-        while '' in paragraph:
-            paragraph.remove('')
-
-    paragraph = nltk.pos_tag(paragraph) # returns tuples with each word paired with parts of speech keyword https://medium.com/@gianpaul.r/tokenization-and-parts-of-speech-pos-tagging-in-pythons-nltk-library-2d30f70af13b
-    new_paragraph = [word[0] for word in paragraph if((word[0] not in stpwrd) and (word[1] in pos_keep))] # the first condition is uneccesary if the second works properly
-
-    new_paragraph = " ".join(new_paragraph)
-    tmp[2] = new_paragraph
-    # once the issue is fixed, I would like to see if lemmatization could be implemented while maintainig reasonable runtime https://www.geeksforgeeks.org/python-lemmatization-with-nltk/
-    return tmp
-
-
+    #par = list(par)
+    
+    paragraph = par[0].lower()#the plots are in the third element of the array. lower is used so that words will still be considered the same instance when vectorized
+    #print(paragraph)
+    paragraph = re.sub('[^+A-Za-z0-9]+',' ',paragraph)#substittes non alphabetic or numerical charectars with a space
+    #print(paragraph)
+    paragraph = re.split(' ',paragraph)#issue mostly fixed (not planning getting it perfect as most noise is reduced), sentence difference can be seen in the print statements in main
+    #print(paragraph)
+    new_paragraph = []
+    pos_keep = ['JJ','JJR','JJR','JJS','RB','RBR','RBS','VB','VBD','VBG','VBN','VBP','VBZ']# all desired parts of speech  (all forms of verbs, adhectives etc. no subject)
+	
+	
+    if('' in paragraph):#removes any empty charectar within a passed paragrph that may be left over
+		while('' in paragraph):
+			paragraph.remove('')
+	
+    	
+    paragraph = nltk.pos_tag(paragraph)#returns tuples with each word paired with parts of speech keyword https://medium.com/@gianpaul.r/tokenization-and-parts-of-speech-pos-tagging-in-pythons-nltk-library-2d30f70af13b
+    #print(paragraph)#the above link shows the keys for the nltk parts of speech
+    new_paragraph = [word[0] for word in paragraph if((word[0] not in stpwrd) and (word[1] in pos_keep))]#the firs condition is uneccesary if the second works properly
+	
+	
+    
+    new_paragraph= " ".join(new_paragraph)
+    
+	#once the issue is fixed, I would like to see if lemmatization could be implemented while maintainig reasonable runtime https://www.geeksforgeeks.org/python-lemmatization-with-nltk/ 	
+	
+    return new_paragraph
 if __name__ == "__main__":
     p = Pool(4)
     seconds = time.time()
-    x_train = pd.read_csv("movie_plots.csv",header=None,usecols=[1,7]) # index, movie names, and plots from movie plot csv
-    y_train = pd.read_csv("movie_plots.csv",header =None,usecols=[5]) # genres
-    #x = [[row[0],row[2]] for row in x_train.itertuples()]
-    #print( re.sub('[^+A-Za-z0-9\\n]+',' ',x_train))
-    #print(x_train)
-    x = list(x_train.itertuples())
-    x = [list(row) for row in x] # converts the pandas dataframe into normal list for compatability with map and overall ease of use
-    del x[0]
-    print(len(x))
-    new_x = list(p.map(Pre, x)) # processes each plot
-    print(new_x[0])
-    print(new_x[1]) # should run through once to see what the elements look like
-    print(new_x[2])
-    print(new_x[5])
-    print(len(new_x))
+    x_tittles = pd.read_csv("movie_plots.csv",header=None,usecols=[1],index_col=0)#tittles
+    x_train = pd.read_csv("movie_plots.csv",header=None,usecols=[7],index_col=0)#plots           
+    y_train = pd.read_csv("movie_plots.csv",header =None,index_col=0,usecols=[5])#genres
+    x_train = list(x_train.itertuples())
+    y_train = list(y_train.itertuples())
+    x_tittles =  list(x_tittles.itertuples())
+    y_train = [list(row) for row in y_train]#converts pandas dataframe into list
+    x_train = [list(x_train[i]) for i in range(len(x_train)) if(y_train[i][0]!="unknown")]#does the same as the list comprehension for y_train for x_train but removing the unknown movies
+
+    x_tittles = [list(x_tittles[i]) for i in range(len(x_tittles)) if(y_train[i][0]!="unknown")]#does the same as the list comprehension for y_train for x_tittles but removing the unknown movies
+    y_train = [y_train[i] for i in range(len(y_train)) if(y_train[i][0]!="unknown")]#removes unknown genre instances
+    #more movies will eventually have to be removed from the training set (movies that share genres or genres that are to niche) for now, this enough to carry through
+    del y_train[0]#removes header statements
+    del x_train[0]
+    del x_tittles[0]
+    movie_set = [copy.deepcopy(x_tittles),copy.deepcopy(x_train),copy.deepcopy(y_train)]#for ease in accessing evenly columned tittle, plot, and genre
+    movie_set[1] = list(p.map(Pre, movie_set[1]))#processes each plot (can be inputed directly in tfidfVectorize().fit_transform as it is a list of strings)
+    """ 
+    print(x_train[0]) 
+    print(movie_set[1][0])
+    print(x_train[3])
+    print(movie_set[1][3])
+    print(x_train[67])
+    print(movie_set[1][67])
+    #x_train is not modified. the difference in the processing can be seen in the execution of the above print statements
+    """
     p.close()
     p.join()
-
+	
     print("finished:")
     print("time taken %f" % (time.time() - seconds))
+    
