731847f930036e0493e2b6b4762dde39e32cf15c
3A-2Z <48304266+3A-2Z@users.noreply.github.com>
2019-04-29 18:46:35 -0400

    Add files via upload

--- rater.py
+++ rater.py
@@ -1,4 +1,4 @@
-#import nltk#the variable corpus is the training set
+
 import re
 import io
 import numpy as np
@@ -17,32 +17,31 @@ predictors = []
 item_profiles = []
 count = 0
 def find_his(user,index,movie_vectors):#extracts user history vectors
-    tmp_count = copy.deepcopy(index)
+    tmp_count = copy.deepcopy(index)#acts as a bookmark for the new user to avoid itterating through all vectors within movie_vectors (rows within the utility matrix)
     user_his = []
     for i in range(len(movie_vectors)):
-        #print("user",user)
-        #print("movie",movie_vectors[tmp_count])
+        
         if(user == movie_vectors[tmp_count][0]):
            
             user_his.append(movie_vectors[tmp_count])
             tmp_count+=1
         else:
-            #tmp_count+=1
+            
             break
     return [user_his,tmp_count]#tmp count is a "boolmark" for where to start the search within the movie_vectors again
-def find_movie(movie,item_profiles):
+def find_movie(movie,item_profiles):#finds the item profile for the desired movie
     ret = []
     for i in item_profiles:
         print(movie)
         if(movie == i[0]):
             ret = copy.deepcopy(i)
             break
-    print(ret)
+   
     del ret[0]
     return ret
-def predict_rate(user_his,curr_user):
+def predict_rate(user_his,curr_user):# was intended to predict the rating for the testing data
     print("begin rating")
-    K = int(math.sqrt(len(user_his)))
+    K = int(math.sqrt(len(user_his)))#chooses k with sqrt heuristic
     movie_cpy = copy.deepcopy(user_his)
     for i in movie_cpy:
         for i in range(3):
@@ -88,11 +87,12 @@ with open('utilityMatrixFinal.csv',encoding = 'utf-8') as util:#parses for utili
             bool+=1
 del predictors[0]
 b = 0
-item_profiles = (row for row in csv.reader(open('itemProfile1.csv',encoding = 'utf-8')))
+item_profiles = (row for row in csv.reader(open('itemProfile1.csv',encoding = 'utf-8')))#attempt at opening large file to prevent memory error
 item_profiles = list(item_profiles)
 del item_profiles[0]
 
 """
+#code below works for parsing item profiles on different machine (was producing memmory error for one of our computers)
 with open('itemProfile1.csv',encoding = 'utf-8') as items:#parses for item profile vectors
     items_read = csv.reader(items)
     bool = 0
@@ -107,19 +107,19 @@ with open('itemProfile1.csv',encoding = 'utf-8') as items:#parses for item profi
             bool+=1
 """
 #del predictors[0]
-curr_user = copy.deepcopy(predictors[0][0])
-curr_user_vec = copy.deepcopy(predictors[0])
+curr_user = copy.deepcopy(predictors[0][0])#gets first user form test set
+curr_user_vec = copy.deepcopy(predictors[0])#gets the test da
 tmp_user_his = []
 index = 0
 tmp = []
 
-tmp = find_his(curr_user,index,movie_vectors)
-tmp_user_his = tmp[0]
+tmp = find_his(curr_user,index,movie_vectors)#finds the first user's history
+tmp_user_his = tmp[0]#list for user history
 index = tmp[1]#user history is gathered for predictor function
 #print(movie_vectors)
 for i in predictors:
     
-    if(i[0] != curr_user):#if
+    if(i[0] != curr_user):#if the user changes in the predictions list,re assign the user history and index
         tmp = find_his(curr_user,index,movie_vectors)#user histor vectors still have the first three elements as the userID, movieID, and rating keethis in mind when finishing the predictor function
         tmp_user_his = tmp[0]
         index = tmp[1]
@@ -131,7 +131,7 @@ for i in predictors:
        predict_rate(tmp_user_his,curr_user_vec) 
     #here aswell with an else statemennt
    
-    break   
+    break   #break statemnt originally for testing
 #print(movie_vectors[index][0])
 #print(len(tmp_user_his))
 print("finished:")
