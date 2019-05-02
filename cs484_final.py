686353b61f519cb33029a02adb7bb04301fe655a
3A-2Z <48304266+3A-2Z@users.noreply.github.com>
2019-05-02 18:30:45 -0400

    Add files via upload

--- cs484_final.py
+++ cs484_final.py
@@ -21,12 +21,13 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from sklearn.ensemble import BaggingClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
 from scipy import sparse
 lem = WordNetLemmatizer()
 stpwrd = set(stopwords.words('english'))
 stpwrd_update = ["br","eof","i","im","i'm",",","it","as","the","this","but","its","it's","you" "also","for","us","was","to","on","there","of","in","his","hers","-"]
 stpwrd.update(stpwrd_update)
-
+DT = DecisionTreeClassifier()
 def Pre(par):
     #par = list(par)
     
@@ -56,27 +57,84 @@ def Pre(par):
 	#once the issue is fixed, I would like to see if lemmatization could be implemented while maintainig reasonable runtime https://www.geeksforgeeks.org/python-lemmatization-with-nltk/ 	
 	
     return new_paragraph
+def remove_all(item,list):
+    for i in list:
+        while(item in i):
+            i.remove(item)
+    return list
 if __name__ == "__main__":
     p = Pool(4)
     seconds = time.time()
     x_tittles = pd.read_csv("movie_plots.csv",header=None,usecols=[1],index_col=0)#tittles
     x_train = pd.read_csv("movie_plots.csv",header=None,usecols=[7],index_col=0)#plots           
     y_train = pd.read_csv("movie_plots.csv",header =None,index_col=0,usecols=[5])#genres
+    
     x_train = list(x_train.itertuples())
     y_train = list(y_train.itertuples())
     x_tittles =  list(x_tittles.itertuples())
     y_train = [list(row)[0] for row in y_train]#converts pandas dataframe into list
+    y_train = [y_train[i] for i in range(len(y_train)) if(y_train[i][0]!="unknown")]
     x_train = [list(x_train[i]) for i in range(len(x_train)) if(y_train[i][0]!="unknown")]#does the same as the list comprehension for y_train for x_train but removing the unknown movies
-    print(len(y_train))
-    print(len(x_train))
-
     x_tittles = [list(x_tittles[i]) for i in range(len(x_tittles)) if(y_train[i][0]!="unknown")]#does the same as the list comprehension for y_train for x_tittles but removing the unknown movies
     y_train = [y_train[i] for i in range(len(y_train)) if(y_train[i][0]!="unknown")]#removes unknown genre instances
-    #more movies will eventually have to be removed from the training set (movies that share genres or genres that are to niche) for now, this enough to carry through
     del y_train[0]#removes header statements
     del x_train[0]
     del x_tittles[0]
+    print(len(y_train))
+    print(len(x_train))
+
+  
+    #more movies will eventually have to be removed from the training set (movies that share genres or genres that are to niche) for now, this enough to carry through
+    print(len(y_train))
+    y_train = set(y_train)
+    y_train = list(y_train)
+    print(len(y_train))
+    genres = [['thriller'],['action'],['adventure'],['fantasy'],['drama'],['comedy'],['romance',['love','romantic']],['sci-fi',['science fiction','sci','fi']],['horror'],['animation'],['documentary'],['family']]#this is the list of genre labels (first element in the list)
+    class_labels = []
+    y_copy = copy.deepcopy(y_train)
+    for i in range(len(y_train)):
+        print(i,y_train[i])
+        y_train[i] = re.sub('[^A-Za-z]+',' ',y_train[i])
+        y_train[i] = y_train[i].split(' ')
+        
+        
+        tmp_list = []
+        for z in range(len(genres)):
+            
+            if len(genres[z]) == 1:
+               # print("t",y_train[i])
+                #print("g",genres[z]) 
+                if genres[z][0] in y_train[i]:
+                    #print("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+                    tmp_list.append(genres[z][0])
+            elif len(genres[z]) == 2:
+                for j in genres[z][1]:
+                    if j in y_train[i]:
+                        #print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+                        tmp_list.append(genres[z][0])
+                        break
+            #print("tmp",tmp_list)
+        if(len(tmp_list) == 0):
+            x_train[i] = 'flag'
+            y_train[i] = 'flag'
+            y_copy[i] = 'flag'
+            x_tittles[i] = 'flag'
+        else:
+            class_labels.append(tmp_list)
+    tmp = remove_all('flag',[y_train,y_copy,x_train,x_tittles])
+    y_train = copy.deepcopy(tmp[0])
+    y_copy = copy.deepcopy(tmp[1])
+    x_train = copy.deepcopy(tmp[2])
+    x_tittles[3] = copy.deepcopy(tmp[3])
+    #for h in range(len(y_copy)):
+        #print(y_copy[h],class_labels[h])
+    #print(len(class_labels))
+    #print(len(y_train))
+    #exit()
+   
+    
     movie_set = [copy.deepcopy(x_tittles),copy.deepcopy(x_train),copy.deepcopy(y_train)]#for ease in accessing evenly columned tittle, plot, and genre
+    #print(movie_set[1])
     movie_set[1] = list(p.map(Pre, movie_set[1]))#processes each plot (can be inputed directly in tfidfVectorize().fit_transform as it is a list of strings)
     
 #    print(x_train[0]) 
@@ -86,17 +144,20 @@ if __name__ == "__main__":
     #print(x_train[67])
     #print(movie_set[1][67])
     #x_train is not modified. the difference in the processing can be seen in the execution of the above print statements
+    movie_set[1] = [movie_set[1][i] for i in range(int(len(movie_set[1])/4))]
+    y_train = [y_train[i] for i in range(int(len(y_train)/4))]
     
     vec = TfidfVectorizer()
     training  = vec.fit_transform(movie_set[1])
     #movie_set[1] = training.get_feature_names()
     #movie_set[1] = sparse.csr_matrix(training.todense())
-    MLP = BaggingClassifier(MLPClassifier(hidden_layer_sizes=(37,)))
+    MLP = MLPClassifier(hidden_layer_sizes=(2,))
     y_train  = np.array(y_train)
     
     print(training.shape)
     print(y_train.shape)
-    MLP.fit(training,y_train)
+    
+    DT.fit(training,y_train)
     #print(training)
     p.close()
     p.join()
