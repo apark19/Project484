e523890a08e59d2fe7900eb808d241b557494a39
Andrew Park <apark19@gmu.edu>
2019-05-11 02:49:44 -0400

    Update 484final_predictorKNN.py

--- 484final_predictorKNN.py
+++ 484final_predictorKNN.py
@@ -1,4 +1,4 @@
-import nltk#creates csv file with parsed genres
+import nltk  # creates csv file with parsed genres
 import re
 import numpy as np
 import sklearn
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy import sparse
 from operator import itemgetter
-from multiprocessing import Process,Lock,Pool,freeze_support
+from multiprocessing import Process, Lock, Pool, freeze_support
 import time
 from nltk import pos_tag
 import pandas as pd
@@ -28,62 +28,76 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 import codecs
 import csv
+from sklearn.neighbors import RadiusNeighborsClassifier
+
 lem = WordNetLemmatizer()
 stpwrd = set(stopwords.words('english'))
-stpwrd_update = ["br","eof","i","im","i'm",",","it","as","the","this","but","its","it's","you" "also","for","us","was","to","on","there","of","in","his","hers","-"]
+stpwrd_update = ["br", "eof", "i", "im", "i'm", ",", "it", "as", "the", "this", "but", "its", "it's", "you", "also",
+                 "for", "us", "was", "to", "on", "there", "of", "in", "his", "hers", "-"]
 stpwrd.update(stpwrd_update)
-#DT = DecisionTreeClassifier()
+# DT = DecisionTreeClassifier()
 vec = TfidfVectorizer()
+
+
 def Pre(par):
-    #par = list(par)
-    
-    paragraph = par.lower()#the plots are in the third element of the array. lower is used so that words will still be considered the same instance when vectorized
-    #print(paragraph)
-    paragraph = re.sub('[^+A-Za-z0-9]+',' ',paragraph)#substittes non alphabetic or numerical charectars with a space
-    #print(paragraph)
-    paragraph = re.split(' ',paragraph)#issue mostly fixed (not planning getting it perfect as most noise is reduced), sentence difference can be seen in the print statements in main
-    #print(paragraph)
+    # par = list(par)
+
+    paragraph = par.lower()  # the plots are in the third element of the array. lower is used so that words will still be considered the same instance when vectorized
+    # print(paragraph)
+    paragraph = re.sub('[^+A-Za-z0-9]+', ' ',
+                       paragraph)  # substittes non alphabetic or numerical charectars with a space
+    # print(paragraph)
+    paragraph = re.split(' ',
+                         paragraph)  # issue mostly fixed (not planning getting it perfect as most noise is reduced), sentence difference can be seen in the print statements in main
+    # print(paragraph)
     new_paragraph = []
-    pos_keep = ['JJ','JJR','JJR','JJS','RB','RBR','RBS','VB','VBD','VBG','VBN','VBP','VBZ']# all desired parts of speech  (all forms of verbs, adhectives etc. no subject)
-	
-	
-    if('' in paragraph):#removes any empty charectar within a passed paragrph that may be left over
-        while('' in paragraph):
-	        paragraph.remove('')
-	
-    	
-    paragraph = nltk.pos_tag(paragraph)#returns tuples with each word paired with parts of speech keyword https://medium.com/@gianpaul.r/tokenization-and-parts-of-speech-pos-tagging-in-pythons-nltk-library-2d30f70af13b
-    #print(paragraph)#the above link shows the keys for the nltk parts of speech
-    #print(paragraph)
-    new_paragraph = [word[0] for word in paragraph if((word[0] not in stpwrd) and (word[1] in pos_keep))]#the firs condition is uneccesary if the second works properly
-	
-	
-    
-    new_paragraph= " ".join(new_paragraph)
-    
-	#once the issue is fixed, I would like to see if lemmatization could be implemented while maintainig reasonable runtime https://www.geeksforgeeks.org/python-lemmatization-with-nltk/ 	
-	
+    pos_keep = ['JJ', 'JJR', 'JJR', 'JJS', 'RB', 'RBR', 'RBS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP',
+                'VBZ']  # all desired parts of speech  (all forms of verbs, adhectives etc. no subject)
+
+    if ('' in paragraph):  # removes any empty charectar within a passed paragrph that may be left over
+        while ('' in paragraph):
+            paragraph.remove('')
+
+    paragraph = nltk.pos_tag(
+        paragraph)  # returns tuples with each word paired with parts of speech keyword https://medium.com/@gianpaul.r/tokenization-and-parts-of-speech-pos-tagging-in-pythons-nltk-library-2d30f70af13b
+    # print(paragraph)#the above link shows the keys for the nltk parts of speech
+    # print(paragraph)
+    new_paragraph = [word[0] for word in paragraph if ((word[0] not in stpwrd) and (
+                word[1] in pos_keep))]  # the firs condition is uneccesary if the second works properly
+
+    new_paragraph = " ".join(new_paragraph)
+
+    # once the issue is fixed, I would like to see if lemmatization could be implemented while maintainig reasonable runtime https://www.geeksforgeeks.org/python-lemmatization-with-nltk/
+
     return new_paragraph
-def KNN(x_data,y_label,p,g_truth,titles):
-   # mlp = BaggingClassifier(MLPClassifier(hidden_layer_sizes=(2,),random_state=0,max_iter=400),n_estimators=5)
-    knn = BaggingClassifier(KNeighborsClassifier(n_neighbors=94),n_estimators = 100)
-    knn.fit(x_data,y_label)
-    pred  = knn.predict(p)
-    score = f1_score(g_truth,pred, average='weighted')
-    with codecs.open('results_knn.csv','w','utf-8') as outfile:
+
+
+def KNN(x_data, y_label, p, g_truth, titles):
+    # mlp = BaggingClassifier(MLPClassifier(hidden_layer_sizes=(2,),random_state=0,max_iter=400),n_estimators=5)
+    knn = BaggingClassifier(KNeighborsClassifier(n_neighbors=95, metric='euclidean'), n_estimators=100)
+    #knn = BaggingClassifier(RadiusNeighborsClassifier(radius=5, n_jobs=-1), n_estimators=100)
+
+    knn.fit(x_data, y_label)
+    pred = knn.predict(p)
+    score = f1_score(g_truth, pred, average='weighted')
+    with codecs.open('results_knn.csv', 'w', 'utf-8') as outfile:
         out = csv.writer(outfile)
-        out.writerow(['title','genre'])
+        out.writerow(['title', 'genre'])
         for i in range(len(pred)):
-            
-            out.writerow([titles[i],pred[i]])
-    #print(pred)
-accuracy = accuracy_score(g_truth, pred, normalize=False)	
-    print("f1: %s" % (score))
-print("accuracy: %s" % (accuracy))	
+            out.writerow([titles[i], pred[i]])
+    # print(pred)
+
+    accuracy = accuracy_score(g_truth, pred, normalize=False)
+    print(score)
+    print(accuracy)
+
+
 def vectorize(data):
     vec = TfidfVectorizer()
-    vector_data  = vec.fit_transform(data)
+    vector_data = vec.fit_transform(data)
     return vector_data
+
+
 if __name__ == "__main__":
     seconds = time.time()
     p = Pool(4)
@@ -93,26 +107,26 @@ if __name__ == "__main__":
             with codecs.open('prediction_dtree.dat','w','utf-8') as predictions:
                 pd.read_csv()
     """
-    train = pd.read_csv('training_data(movies).csv',index_col = 0)
-    test = pd.read_csv('movies_2020.csv',)
+    train = pd.read_csv('final_movies_training.csv', index_col=0)
+    test = pd.read_csv('testFinal.csv', )
     train_x = [row for row in train['plot']]
     train_y = np.array([row for row in train['genre']])
     train_x = np.array(train_x)
-    titles = [row for row in test['tiitle']]
-   
-    test_x = [ row  for row in test['plot']]
-    g_truth = [ row.lower()  for row in test['genre']]
-   
-    test_x = np.array(list(p.map(Pre,test_x)))
+    titles = [row for row in test['title']]
+
+    test_x = [row for row in test['plot']]
+    g_truth = [row.lower() for row in test['genre']]
+
+    test_x = np.array(list(p.map(Pre, test_x)))
     training = vec.fit_transform(train_x)
     testing = vec.transform(test_x)
     print(training.shape)
     print(testing.shape)
-    KNN(training,train_y,testing,g_truth,titles)
-    
-    #print(test_x)
-    #exit()
+    KNN(training, train_y, testing, g_truth, titles)
+
+    # print(test_x)
+    # exit()
     print("finished:")
     print("time taken %f" % (time.time() - seconds))
-    
-    
+
+
